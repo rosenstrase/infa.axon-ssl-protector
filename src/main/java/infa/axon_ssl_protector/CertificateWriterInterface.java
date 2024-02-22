5500bc1f2536f115a3fcbc9ee26b5b4b63d8f9c5
rosenstrase <neil.porter@gds2ts.onmicrosoft.com>
2024-02-22 14:06:30 +0100

    Latest commit.

--- src/main/java/infa/axon_ssl_protector/CertificateWriterInterface.java
+++
@@ -1,9 +0,0 @@
-package infa.axon_ssl_protector;
-
-import java.security.cert.X509Certificate;
-
-public interface CertificateWriterInterface {
-
-	public void writeCertificateToFile(X509Certificate certificate, String fileName, String fileExtension);
-
-}
--- src/main/java/infa/axon_ssl_protector/SignatureAlgorithm.java
+++
@@ -1,7 +0,0 @@
-package infa.axon_ssl_protector;
-
-public enum SignatureAlgorithm {
-
-	RSA;
-
-}
