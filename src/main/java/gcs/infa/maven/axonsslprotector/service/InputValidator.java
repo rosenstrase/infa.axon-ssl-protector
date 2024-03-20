fd038c22c7009fb9b01c5c7487472e1433676f78
rosenstrase <neil.porter@gds2ts.onmicrosoft.com>
2024-03-20 15:52:41 +0100

    Added private void loadValidCountryCodes(String jsonFilePath) to InputValidator Added Google Gson dependency to pom.xml

--- src/main/java/gcs/infa/maven/axonsslprotector/service/InputValidator.java
+++ src/main/java/gcs/infa/maven/axonsslprotector/service/InputValidator.java
@@ -1,6 +1,44 @@
 package gcs.infa.maven.axonsslprotector.service;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 public class InputValidator implements ValidationService {
+
+	private static final int MAX_COUNTRY_CODE_LENGTH = 2;
+	private Set<String> validCountryCodes;
+
+	public InputValidator() {
+		// Load country codes from a JSON file during object construction
+		loadValidCountryCodes("/axon-ssl-protector/ISO3166-1.alpha2.json");
+	}
+
+	private void loadValidCountryCodes(String jsonFilePath) {
+		try {
+			InputStream inputStream = getClass().getResourceAsStream(jsonFilePath);
+			InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+			JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
+			validCountryCodes = new HashSet<>();
+			JsonArray countryCodesArray = jsonObject.getAsJsonArray("validCountryCodes");
+			for (int i = 0; i < countryCodesArray.size(); i++) {
+				String countryCode = countryCodesArray.get(i).getAsString();
+				if (countryCode.length() <= MAX_COUNTRY_CODE_LENGTH) {
+					validCountryCodes.add(countryCode);
+				} else {
+					System.err.println("Invalid country code: " + countryCode);
+				}
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to load country codes", e);
+		}
+	}
 
 	// PATTERNS
 	private static final String EMAIL_PATTERN = "^[A-Za-z0-9+_.-]+@(.+)$";
